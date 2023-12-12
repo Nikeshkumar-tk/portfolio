3e95848d3c51fb384c492cc38ba2db6341556cee
Nikeshkumar-tk <104181447+Nikeshkumar-tk@users.noreply.github.com>
2023-12-12 21:31:11 +0530

    first commit

---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,23 @@
+import { siteConfig } from "@/config/site";
+
+export default function Navbar() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="flex items-center px-20 justify-center">
+        <NavLinks />
+      </div>
+    </header>
+  );
+}
+
+function NavLinks() {
+  return (
+    <div className="flex h-14 items-center gap-9">
+      {siteConfig.nav_links.map((link) => (
+        <a href={link.link} className="line-clamp-2 text-lg leading-snug text-gray-700 font-mono">
+          {link.header}
+        </a>
+      ))}
+    </div>
+  );
+}
---
+++ src/config/site.ts
@@ -0,0 +1,21 @@
+export const siteConfig = {
+    name: "Nikesh kumar T K",
+    nav_links: [
+        {
+            header: "About_me",
+            link: "/"
+        },
+        {
+            header: "Projects",
+            link: "/projects"
+        },
+        {
+            header: "Blogs",
+            link: "/blogs"
+        },
+        {
+            header: "Contact",
+            link: "/blogs"
+        },
+    ]
+}
